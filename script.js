6ea48086e96362b35c423c4b79b33b417179a525
Ryan Duran <dogoathena@gmail.com>
2024-02-08 10:43:16 -0800

    5th

--- script.js
+++ script.js
@@ -1,4 +1,49 @@
-let certname = document.getElementById()
-function generate() {
+document.addEventListener("DOMContentLoaded", function () {
 
-}
+    const textEl = document.getElementById('css-code-holder').innerText
+    let text = document.getElementById('css-text')
+    let speed = 100
+    let idx = 0;
+
+    writeText()
+
+
+    function writeText() {
+        if(textEl[idx] == ' ') {
+            idx++;
+            return(writeText())
+        }
+        text.innerText = textEl.slice(0,idx)
+
+        idx++;
+        if(idx < textEl.length) {
+            console.log(idx);
+            return(setTimeout(writeText, speed))
+            
+        }
+        else {
+            return
+        }
+    }
+
+})
+
+// document.addEventListener("DOMContentLoaded", function () {
+
+//     const textEl = document.getElementById('css-code').innerText;
+//     let text = document.getElementById('css-text').innerText;
+//     let speed = 200;
+
+//     console.log(text);
+
+//     writeText();
+
+//     function writeText() {
+//         for(idx = 0; idx < text.length; idx++) {
+//             text = textEl.slice(0,idx);
+//             console.log(text);
+//         }
+//         setTimeout(writeText, speed);
+//     }
+
+// })

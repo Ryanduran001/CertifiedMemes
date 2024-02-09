4f70976070edb9677fbbd89f7555c3bf956c5ab7
Ryan Duran <dogoathena@gmail.com>
2024-02-09 10:40:39 -0800

    6th

--- script.js
+++ script.js
@@ -1,24 +1,51 @@
 document.addEventListener("DOMContentLoaded", function () {
-
-    const textEl = document.getElementById('css-code-holder').innerText
-    let text = document.getElementById('css-text')
     let speed = 100
     let idx = 0;
 
-    writeText()
+
+    const css_code_holder = document.getElementById('css-code-holder').innerText
+    let css_code = document.getElementById('css-text')
+
+    writeText(css_code, css_code_holder)
+
+    headertext_holder = document.getElementById('headertext-holder').innerText
+    headertext = document.getElementById('headertext')
 
 
-    function writeText() {
-        if(textEl[idx] == ' ') {
+    thisis_holder = document.getElementById('thisis-holder').innerText
+    thisis = document.getElementById('thisis')
+
+
+    namecert_holder = document.getElementById('namecert-holder').innerText
+    namecert = document.getElementById('namecert')
+    
+
+    certifier_holder = document.getElementById('certifier-holder').innerText
+    certifier = document.getElementById('certifier')
+    
+
+    certifier_position_holder = document.getElementById('certifier-position-holder').innerText
+    certifier_position = document.getElementById('certifier-position')
+
+
+    issue_date_holder = document.getElementById('issue-date-holder').innerText
+    issue_date = document.getElementById('issue-date')
+
+
+    function writeText(id,text,idx = 0) {
+        let element = document.getElementById(id);
+        let holder = document.getElementById(text);
+
+        if(holder[idx] == ' ') {
             idx++;
-            return(writeText())
+            return(writeText(element, holder))
         }
-        text.innerText = textEl.slice(0,idx)
+        element.innerText = holder.slice(0,idx)
 
         idx++;
-        if(idx < textEl.length) {
+        if(idx < holder.length) {
             console.log(idx);
-            return(setTimeout(writeText, speed))
+            return(setTimeout(writeText, speed, element, holder))
             
         }
         else {
@@ -26,24 +53,11 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
+    function generate() {
+
+    }
+
+
+   
+
 })
-
-// document.addEventListener("DOMContentLoaded", function () {
-
-//     const textEl = document.getElementById('css-code').innerText;
-//     let text = document.getElementById('css-text').innerText;
-//     let speed = 200;
-
-//     console.log(text);
-
-//     writeText();
-
-//     function writeText() {
-//         for(idx = 0; idx < text.length; idx++) {
-//             text = textEl.slice(0,idx);
-//             console.log(text);
-//         }
-//         setTimeout(writeText, speed);
-//     }
-
-// })
